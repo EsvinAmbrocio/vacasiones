ca1de55f59b3dd63455f448de475a31c6f0afc17
Esvin Ambrocio <esvinambrocioc@gmail.com>
2024-05-05 08:33:10 -0600

    feat: cors problem

--- backend/public/index.php
+++ backend/public/index.php
@@ -1,5 +1,12 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: *");
+if ($_SERVER["REQUEST_METHOD"] == 'OPTIONS') {
+    header("Access-Control-Allow-Origin: " . $_SERVER["HTTP_ORIGIN"]);
+    header("Access-Control-Allow-Methods: *");
+    http_response_code(200);
+}
 echo "<pre>";
-var_dump($_SERVER);
+var_dump($_SERVER["REQUEST_METHOD"]);
+var_dump($_SERVER["HTTP_ORIGIN"]?? '*');
 echo "</pre>";
-
--- frontend/app.js
+++ frontend/app.js
@@ -1,2 +1,31 @@
+const URL_API = 'http://localhost:7000/'
+
 document.addEventListener('DOMContentLoaded', (e) => {
+
+  const vacationRequestInput = document.querySelector('#vacation-request')
+  if(!(vacationRequestInput instanceof HTMLFormElement)){
+    throw new Error('No existe el formulario')
+  }
+
+  vacationRequestInput.addEventListener('submit', (e)=> {
+    e.preventDefault()
+    const form = new FormData(vacationRequestInput)
+  })
+
+  const nameInput = vacationRequestInput.querySelector('#name')
+  if(!(nameInput instanceof HTMLInputElement)){
+    throw new Error('No existe el input de nombre')
+  }
+  nameInput.addEventListener('keyup', ()=> {
+    if(!Boolean(nameInput.value)){
+      return;
+    }
+    fetch(URL_API, {
+      method: 'PUT'
+    }).then((response)=> {
+      console.log(response)
+    }).catch((e) => {
+      console.log(e)
+    })
+  })
 })
